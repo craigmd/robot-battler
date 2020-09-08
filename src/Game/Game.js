0af72c39128cce82bd7ab8d92f5ba8bc19579211
craigmd <craigmichaeldickson@gmail.com>
2020-09-07 22:31:17 -0400

    Add StatusBar component

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -1,51 +1,19 @@
 import React from 'react';
 
+import { StatusBar } from '../StatusBar/StatusBar';
+
 import './Game.css';
 
 export function Game() {
   return (
     <main className="game">
       <section className="opponent">
-        <section className="status-bar">
-          <div className="actions-left"></div>
-          <div className="status-center">
-            <p className="deck-size">Deck: 23</p>
-            <p className="hand-size">Hand: 7</p>
-            <p className="action-count">Actions: 4</p>
-          </div>
-          <div className="actions-right"></div>
-        </section>
+        <StatusBar isYou={false} deckSize={23} handSize={7} actionsCount={4} />
         <section className="battle-line"></section>
       </section>
       <section className="you">
         <section className="battle-line"></section>
-        <section className="status-bar">
-          <div className="actions-left">
-            <button
-              className="button action"
-              type="button"
-              name="draw"
-              onClick={() => console.log('draw')}
-            >
-              Draw
-            </button>
-          </div>
-          <div className="status-center">
-            <p className="deck-size">Deck: 23</p>
-            <p className="hand-size">Hand: 7</p>
-            <p className="action-count">Actions: 4</p>
-          </div>
-          <div className="actions-right">
-            <button
-              className="button action"
-              type="button"
-              name="end-turn"
-              onClick={() => console.log('end turn')}
-            >
-              End turn
-            </button>
-          </div>
-        </section>
+        <StatusBar isYou={true} deckSize={23} handSize={7} actionsCount={4} />
         <section className="workshop">
           <section className="hand"></section>
         </section>
